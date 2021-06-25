4c0e956d434634012475960839585228f03c3200
Michelle Mannering <36594527+MishManners@users.noreply.github.com>
2021-06-25 13:31:01 +1000

    Update server.py

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ This is a simple Battlesnake server written in Python.
 For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
 """
 
+# Code for the Sneki Snek Battlesnake
+
 class Battlesnake(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
